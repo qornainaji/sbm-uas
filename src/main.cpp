52858ab565d6d22e8bc14bde132e74796e382258
mdaniyalk <muchammad.daniyal.kautsar@mail.ugm.ac.id>
2023-06-18 12:56:25 +0700

    fix: mqtt connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
-#include <Arduino.h>
+// #include <Arduino.h>
+#include <vector>
+
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <PubSubClient.h>
+
 #include "SPIFFS.h"
 #include <Arduino_JSON.h>
 #include "DHTesp.h"
+
+
+
+
 // #include <Adafruit_BME280.h>
 // #include <Adafruit_Sensor.h>
 
 // Replace with your network credentials
-const char* ssid = "uswaw";
-const char* password = "esklimenaq";
+const char* ssid = "LARELA272_ATAS";
+const char* password = "putra272";
+
+// Add your MQTT Broker IP address, example:
+const char* mqtt_server = "192.168.20.13";
+// const char* mqtt_server = "127.0.0.1";
+const int mqttPort = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -41,8 +58,10 @@ const int potpin = 34;
 const int butright = 26;
 const int butleft = 27;
 
-int value = 0;
+unsigned long value = 0;
 int press = 0;
+
+const int ledPin = 4;
 
 DHTesp dhtSensor;
 
@@ -54,12 +73,23 @@ DHTesp dhtSensor;
 // }
 
 // Get Sensor Readings and return JSON object
-String getSensorReadings(){
-  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
-  readings["temperature"] = String(data.temperature, 2);
-  readings["humidity"] =  String(data.humidity, 1);
-  readings["potentiometer"] = String(value/40.95);
-  readings["tiltmeter"] = String(press);
+std::vector<String> getSensorReadings() {
+  TempAndHumidity data = dhtSensor.getTempAndHumidity();
+  std::vector<String> sensorReadings = {
+    String(data.temperature, 2),
+    String(data.humidity, 1),
+    String(value*0.02442002442, 2),
+    String(press)
+  };
+  return sensorReadings;
+}
+
+String converttoJson(){
+  std::vector<String> sensorReading = getSensorReadings();
+  readings["temperature"] = sensorReading[0];
+  readings["humidity"] =  sensorReading[1];
+  readings["potentiometer"] = sensorReading[2];
+  readings["tiltmeter"] = sensorReading[3]; 
   String jsonString = JSON.stringify(readings);
   return jsonString;
 }
@@ -74,7 +104,7 @@ void initSPIFFS() {
 
 // Initialize WiFi
 void initWiFi() {
-  WiFi.mode(WIFI_STA);
+  // WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
@@ -82,6 +112,35 @@ void initWiFi() {
     delay(1000);
   }
   Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
 }
 
 void setup() {
@@ -93,6 +152,10 @@ void setup() {
   initWiFi();
   initSPIFFS();
 
+  // Init mqtt connection
+  client.setServer(mqtt_server, mqttPort);
+  client.setCallback(callback);
+
   // Web Server Root URL
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", "text/html");
@@ -102,7 +165,7 @@ void setup() {
 
   // Request for the latest sensor readings
   server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request){
-    String json = getSensorReadings();
+    String json = converttoJson();
     request->send(200, "application/json", json);
     json = String();
   });
@@ -121,7 +184,31 @@ void setup() {
   server.begin();
 }
 
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+    // if (client.connect()) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
   if ((millis() - lastTime) > timerDelay) {
     // Send Events to the client with the Sensor Readings Every 1/2 seconds
     value = analogRead(potpin);
@@ -150,7 +237,34 @@ void loop() {
 
     Serial.println(press);
     events.send("ping",NULL,millis());
-    events.send(getSensorReadings().c_str(),"new_readings" ,millis());
+
+    std::vector<String> sensorReading = getSensorReadings();
+    readings["temperature"] = sensorReading[0];
+    readings["humidity"] =  sensorReading[1];
+    readings["potentiometer"] = sensorReading[2];
+    readings["tiltmeter"] = sensorReading[3]; 
+    String jsonString = JSON.stringify(readings);
+
+    events.send(jsonString.c_str(),"new_readings" ,millis());
+    
+
+    Serial.print("Temperature: ");
+    Serial.println(readings["temperature"]);
+    client.publish("esp32/temperature", readings["temperature"]);
+
+    Serial.print("Humidity: ");
+    Serial.println(readings["humidity"]);
+    client.publish("esp32/humidity", readings["humidity"]);
+
+    Serial.print("Potentiometer: ");
+    Serial.println(readings["potentiometer"]);
+    client.publish("esp32/potentiometer", readings["potentiometer"]);
+
+    Serial.print("Tiltmeter: ");
+    Serial.println(readings["tiltmeter"]);
+    client.publish("esp32/tiltmeter", readings["tiltmeter"]);
+
+
     lastTime = millis();
   }
 }
